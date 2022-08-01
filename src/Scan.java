d8a64ae2b58ed2c51ac5a5b0c260a6e00f88f1e6
camilo139801 <Juan.Benavides@endava.com>
2022-08-01 15:58:39 -0500

    se agrego un gracias por todo

--- src/Scan.java
+++ src/Scan.java
@@ -28,6 +28,6 @@ public class Scan {
         } else{
           System.out.println("El Estudiante: " + Estudiante + "Reprovo con: " + promedio);
         }
-        System.out.println("Gracias: ");
+        System.out.println("Gracias por todo: ");
     }
 }
